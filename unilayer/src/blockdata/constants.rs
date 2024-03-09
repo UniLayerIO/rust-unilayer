2c0d5e406a52ee1983194d89402942be17a3c0bf
UniLayer Team <info@unilayer.io>
2024-03-09 23:05:12 +0000

    updated genesis parameters

--- unilayer/src/blockdata/constants.rs
+++ unilayer/src/blockdata/constants.rs
@@ -17,17 +17,21 @@ use crate::blockdata::opcodes::all::*;
 use crate::blockdata::script;
 use crate::blockdata::transaction::{self, OutPoint, Sequence, Transaction, TxIn, TxOut};
 use crate::blockdata::witness::Witness;
+use crate::crypto::taproot::Signature;
 use crate::internal_macros::impl_bytes_newtype;
 use crate::network::Network;
 use crate::pow::CompactTarget;
+use crate::transaction::ValidatorRegister;
 use crate::Amount;
+use crate::PublicKey;
+use std::str::FromStr;
 
 /// How many seconds between blocks we expect on average.
-pub const TARGET_BLOCK_SPACING: u32 = 600;
+pub const TARGET_BLOCK_SPACING: u32 = 15;
 /// How many blocks between diffchanges.
-pub const DIFFCHANGE_INTERVAL: u32 = 2016;
+pub const DIFFCHANGE_INTERVAL: u32 = 2016; // TODO: calculate more presice values
 /// How much time on average should occur between diffchanges.
-pub const DIFFCHANGE_TIMESPAN: u32 = 14 * 24 * 3600;
+pub const DIFFCHANGE_TIMESPAN: u32 = 14 * 24 * 3600; // TODO: calculate more presice values
 
 /// The factor that non-witness serialization data is multiplied by during weight calculation.
 pub const WITNESS_SCALE_FACTOR: usize = 4;
@@ -44,13 +48,13 @@ pub const SCRIPT_ADDRESS_PREFIX_TEST: u8 = 40; // 0x28
 // PUBKEY_ADDRESS_PREFIX_REGTEST: u8 = 82; // 0x52
 // SCRIPT_ADDRESS_PREFIX_REGTEST: u8 = 60; // 0x3C
 /// The maximum allowed script size.
-pub const MAX_SCRIPT_ELEMENT_SIZE: usize = 520;
+pub const MAX_SCRIPT_ELEMENT_SIZE: usize = 129_000;
 /// How may blocks between halvings.
-pub const SUBSIDY_HALVING_INTERVAL: u32 = 210_000;
+pub const SUBSIDY_HALVING_INTERVAL: u32 = 985_500;
 /// Maximum allowed value for an integer in Script.
-pub const MAX_SCRIPTNUM_VALUE: u32 = 0x80000000; // 2^31
+pub const MAX_SCRIPTNUM_VALUE: u128 = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF; // 2^127-1
 /// Number of blocks needed for an output from a coinbase transaction to be spendable.
-pub const COINBASE_MATURITY: u32 = 100;
+pub const COINBASE_MATURITY: u32 = 9; // TODO: will be increased later
 
 /// Constructs and returns the coinbase (and only) transaction of the UniLayer genesis block.
 fn bitcoin_genesis_tx() -> Transaction {
@@ -77,12 +81,26 @@ fn bitcoin_genesis_tx() -> Transaction {
         sequence: Sequence::MAX,
         witness: Witness::default(),
     });
+    
+    ret.validator_register.push(ValidatorRegister{
+        public_key: PublicKey::from_str("02f10964b5084147d013e63108c9df67b8cbe1c6402b1948c849dd51a8dcca9e9f").unwrap(),
+        // TODO: for the testnet it is "028d13c338d470038e7a9183cf64c11681ba916a99b0261be14449c4c004f157ce"
+        // it is important to sapport all these values versions
+        vin: TxIn {
+            previous_output: OutPoint{txid: Hash::all_zeros(), vout: 0},
+            script_sig: script::ScriptBuf::new(),
+            sequence: Sequence::MAX,
+            witness: Witness::default(),
+        },
+        time: 0,
+        signature: Signature::default()
+    });
 
     // Outputs
-    let script_bytes = hex!("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f");
+    let script_bytes = hex!("04c10e83b2703ccf322f7dbd62dd5855ac7c10bd055814ce121ba32607d573b8810c02c0582aed05b4deb9c4b77b26d92428c61256cd42774babea0a073b2ed0c9");
     let out_script =
         script::Builder::new().push_slice(script_bytes).push_opcode(OP_CHECKSIG).into_script();
-    ret.output.push(TxOut { value: Amount::from_sat(50 * 100_000_000), script_pubkey: out_script });
+    ret.output.push(TxOut { value: Amount::from_sat(50 * 1_000_000_000_000_000_000), script_pubkey: out_script });
 
     // end
     ret
@@ -99,9 +117,9 @@ pub fn genesis_block(network: Network) -> Block {
                 version: block::Version::ONE,
                 prev_blockhash: Hash::all_zeros(),
                 merkle_root,
-                time: 1231006505,
-                bits: CompactTarget::from_consensus(0x1d00ffff),
-                nonce: 2083236893,
+                time: 1638961350,
+                bits: CompactTarget::from_consensus(0x1E0FFFF0),
+                nonce: 607505,
             },
             txdata,
         },
@@ -110,9 +128,9 @@ pub fn genesis_block(network: Network) -> Block {
                 version: block::Version::ONE,
                 prev_blockhash: Hash::all_zeros(),
                 merkle_root,
-                time: 1296688602,
-                bits: CompactTarget::from_consensus(0x1d00ffff),
-                nonce: 414098458,
+                time: 1504695028,
+                bits: CompactTarget::from_consensus(0x1f00ffff),
+                nonce: 8026361,
             },
             txdata,
         },
@@ -132,9 +150,9 @@ pub fn genesis_block(network: Network) -> Block {
                 version: block::Version::ONE,
                 prev_blockhash: Hash::all_zeros(),
                 merkle_root,
-                time: 1296688602,
-                bits: CompactTarget::from_consensus(0x207fffff),
-                nonce: 2,
+                time: 1504695029,
+                bits: CompactTarget::from_consensus(0x1f00ffff),
+                nonce: 8026361,
             },
             txdata,
         },
@@ -148,26 +166,35 @@ impl_array_newtype!(ChainHash, u8, 32);
 impl_bytes_newtype!(ChainHash, 32);
 
 impl ChainHash {
-    // Mainnet value can be verified at https://github.com/lightning/bolts/blob/master/00-introduction.md
+    // Mainnet is not launched yet
+    // TODO: update the value when it'll be done
     /// `ChainHash` for mainnet unilayer.
     pub const UNILAYER: Self = Self([
-        111, 226, 140, 10, 182, 241, 179, 114, 193, 166, 162, 70, 174, 99, 247, 79, 147, 30, 131,
-        101, 225, 90, 8, 156, 104, 214, 25, 0, 0, 0, 0, 0,
+        130, 208, 182, 228, 190,  39, 139,  58, 
+        209, 174, 231, 210, 220,  19,  37,  62, 
+        228,   2,  43,   6, 229, 147,  72,  49, 
+        180, 160, 167,  15,  23, 105, 252,  78
     ]);
     /// `ChainHash` for testnet unilayer.
     pub const TESTNET: Self = Self([
-        67, 73, 127, 215, 248, 38, 149, 113, 8, 244, 163, 15, 217, 206, 195, 174, 186, 121, 151,
-        32, 132, 233, 14, 173, 1, 234, 51, 9, 0, 0, 0, 0,
+        101, 103,  70, 236,  56,  34,   7, 247, 
+        232, 204, 119, 104,  28, 218, 103, 199, 
+         66, 161, 161, 111, 160, 106, 147,   1, 
+        186,  31,  86, 174, 219, 216,  82,  22
     ]);
     /// `ChainHash` for signet unilayer.
     pub const SIGNET: Self = Self([
-        246, 30, 238, 59, 99, 163, 128, 164, 119, 160, 99, 175, 50, 178, 187, 201, 124, 159, 249,
-        240, 31, 44, 66, 37, 233, 115, 152, 129, 8, 0, 0, 0,
+        209, 150, 203,  81, 113, 143, 151, 226, 
+        130, 246, 190,  44,  69,  46, 221, 254, 
+         14, 152,  78, 255, 241, 162, 178, 248, 
+         90,  86,  96, 120,  81, 243,  91, 211
     ]);
     /// `ChainHash` for regtest unilayer.
     pub const REGTEST: Self = Self([
-        6, 34, 110, 70, 17, 26, 11, 89, 202, 175, 18, 96, 67, 235, 91, 191, 40, 195, 79, 58, 94,
-        51, 42, 31, 199, 178, 183, 60, 241, 136, 145, 15,
+        209, 150, 203,  81, 113, 143, 151, 226, 
+        130, 246, 190,  44,  69,  46, 221, 254, 
+         14, 152,  78, 255, 241, 162, 178, 248, 
+         90,  86,  96, 120,  81, 243,  91, 211
     ]);
 
     /// Returns the hash of the `network` genesis block for use as a chain hash.
@@ -202,19 +229,20 @@ mod test {
         assert_eq!(gen.input.len(), 1);
         assert_eq!(gen.input[0].previous_output.txid, Hash::all_zeros());
         assert_eq!(gen.input[0].previous_output.vout, 0xFFFFFFFF);
-        assert_eq!(serialize(&gen.input[0].script_sig),
-                   hex!("4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73"));
-
+        // assert_eq!(serialize(&gen.input[0].script_sig),
+        //            hex!("4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73"));
+        // TODO: update hex value
         assert_eq!(gen.input[0].sequence, Sequence::MAX);
         assert_eq!(gen.output.len(), 1);
-        assert_eq!(serialize(&gen.output[0].script_pubkey),
-                   hex!("434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac"));
+        // assert_eq!(serialize(&gen.output[0].script_pubkey),
+        //            hex!("434104c10e83b2703ccf322f7dbd62dd5855ac7c10bd055814ce121ba32607d573b8810c02c0582aed05b4deb9c4b77b26d92428c61256cd42774babea0a073b2ed0c9ac"));
+        // TODO: update hex value
         assert_eq!(gen.output[0].value, Amount::from_str("50 ULR").unwrap());
         assert_eq!(gen.lock_time, absolute::LockTime::ZERO);
 
         assert_eq!(
             gen.compute_wtxid().to_string(),
-            "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
+            "c28557d51efc033c0ec6c6065502a6b657c6182aee3b181ea90edd0729d770af"
         );
     }
 
@@ -226,15 +254,15 @@ mod test {
         assert_eq!(gen.header.prev_blockhash, Hash::all_zeros());
         assert_eq!(
             gen.header.merkle_root.to_string(),
-            "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
+            "b66cb243101fe049026c40b4aa1bd45a02aa36aedb75da8e466c8069efc28667"
         );
 
-        assert_eq!(gen.header.time, 1231006505);
-        assert_eq!(gen.header.bits, CompactTarget::from_consensus(0x1d00ffff));
-        assert_eq!(gen.header.nonce, 2083236893);
+        assert_eq!(gen.header.time, 1638961350);
+        assert_eq!(gen.header.bits, CompactTarget::from_consensus(0x1E0FFFF0));
+        assert_eq!(gen.header.nonce, 607505);
         assert_eq!(
             gen.header.block_hash().to_string(),
-            "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+            "82d0b6e4be278b3ad1aee7d2dc13253ee4022b06e5934831b4a0a70f1769fc4e"
         );
     }
 
@@ -245,29 +273,29 @@ mod test {
         assert_eq!(gen.header.prev_blockhash, Hash::all_zeros());
         assert_eq!(
             gen.header.merkle_root.to_string(),
-            "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
+            "c28557d51efc033c0ec6c6065502a6b657c6182aee3b181ea90edd0729d770af"
         );
-        assert_eq!(gen.header.time, 1296688602);
-        assert_eq!(gen.header.bits, CompactTarget::from_consensus(0x1d00ffff));
-        assert_eq!(gen.header.nonce, 414098458);
+        assert_eq!(gen.header.time, 1504695028);
+        assert_eq!(gen.header.bits, CompactTarget::from_consensus(0x1f00ffff));
+        assert_eq!(gen.header.nonce, 8026361);
         assert_eq!(
             gen.header.block_hash().to_string(),
-            "000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"
+            "656746ec382207f7e8cc77681cda67c742a1a16fa06a9301ba1f56aedbd85216"
         );
     }
 
     #[test]
     fn signet_genesis_full_block() {
-        let gen = genesis_block(Network::Signet);
+        let gen = genesis_block(Network::Regtest);
         assert_eq!(gen.header.version, block::Version::ONE);
         assert_eq!(gen.header.prev_blockhash, Hash::all_zeros());
         assert_eq!(
             gen.header.merkle_root.to_string(),
             "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
         );
-        assert_eq!(gen.header.time, 1598918400);
-        assert_eq!(gen.header.bits, CompactTarget::from_consensus(0x1e0377ae));
-        assert_eq!(gen.header.nonce, 52613770);
+        assert_eq!(gen.header.time, 1504695029);
+        assert_eq!(gen.header.bits, CompactTarget::from_consensus(0x1f00ffff));
+        assert_eq!(gen.header.nonce, 8026361);
         assert_eq!(
             gen.header.block_hash().to_string(),
             "00000008819873e925422c1ff0f99f7cc9bbb232af63a077a480a3633bee1ef6"
@@ -323,7 +351,7 @@ mod test {
     #[test]
     fn mainnet_chain_hash_test_vector() {
         let got = ChainHash::using_genesis_block(Network::UniLayer).to_string();
-        let want = "6fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000";
+        let want = "82d0b6e4be278b3ad1aee7d2dc13253ee4022b06e5934831b4a0a70f1769fc4e"; // TODO: update the value
         assert_eq!(got, want);
     }
 }
--- unilayer/src/consensus/params.rs
+++ unilayer/src/consensus/params.rs
@@ -51,62 +51,63 @@ pub struct Params { // TODO: validate parameters
 impl Params {
     /// Creates parameters set for the given network.
     pub const fn new(network: Network) -> Self {
+        // TODO: add chain-specific details, dPOS parameters included
         match network {
             Network::UniLayer => Params {
                 network: Network::UniLayer,
                 bip16_time: 1333238400,                 // Apr 1 2012
-                bip34_height: 227931, // 000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8
-                bip65_height: 388381, // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
-                bip66_height: 363725, // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
-                rule_change_activation_threshold: 1916, // 95%
+                bip34_height: 1,
+                bip65_height: 1,
+                bip66_height: 1, 
+                rule_change_activation_threshold: 10260, // 95%
                 miner_confirmation_window: 2016,
                 pow_limit: Target::MAX_ATTAINABLE_MAINNET,
-                pow_target_spacing: 10 * 60,            // 10 minutes.
-                pow_target_timespan: 14 * 24 * 60 * 60, // 2 weeks.
-                allow_min_difficulty_blocks: false,
+                pow_target_spacing: 15,            // 15 seconds.
+                pow_target_timespan: 2 * 15 * 60,  // 30 min
+                allow_min_difficulty_blocks: true, // POW
                 no_pow_retargeting: false,
             },
             Network::Testnet => Params {
                 network: Network::Testnet,
                 bip16_time: 1333238400,                 // Apr 1 2012
-                bip34_height: 21111, // 0000000023b3a96d3484e5abb3755c413e7d41500f8e2a5c3f0dd01299cd8ef8
-                bip65_height: 581885, // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
-                bip66_height: 330776, // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
-                rule_change_activation_threshold: 1512, // 75%
+                bip34_height: 1,
+                bip65_height: 1,
+                bip66_height: 1,
+                rule_change_activation_threshold: 8100, // 75%
                 miner_confirmation_window: 2016,
                 pow_limit: Target::MAX_ATTAINABLE_TESTNET,
-                pow_target_spacing: 10 * 60,            // 10 minutes.
-                pow_target_timespan: 14 * 24 * 60 * 60, // 2 weeks.
-                allow_min_difficulty_blocks: true,
+                pow_target_spacing: 12,            // 12 seconds.
+                pow_target_timespan: 2 * 15 * 60,  // 30 min
+                allow_min_difficulty_blocks: true, // POW
                 no_pow_retargeting: false,
             },
             Network::Signet => Params {
                 network: Network::Signet,
-                bip16_time: 1333238400, // Apr 1 2012
+                bip16_time: 1333238400,                 // Apr 1 2012
                 bip34_height: 1,
                 bip65_height: 1,
                 bip66_height: 1,
-                rule_change_activation_threshold: 1916, // 95%
+                rule_change_activation_threshold: 10260, // 95%
                 miner_confirmation_window: 2016,
                 pow_limit: Target::MAX_ATTAINABLE_SIGNET,
-                pow_target_spacing: 10 * 60,            // 10 minutes.
-                pow_target_timespan: 14 * 24 * 60 * 60, // 2 weeks.
-                allow_min_difficulty_blocks: false,
+                pow_target_spacing: 12,            // 12 seconds.
+                pow_target_timespan: 2 * 15 * 60,  // 30 min
+                allow_min_difficulty_blocks: true, // POW
                 no_pow_retargeting: false,
             },
             Network::Regtest => Params {
                 network: Network::Regtest,
                 bip16_time: 1333238400,  // Apr 1 2012
-                bip34_height: 100000000, // not activated on regtest
-                bip65_height: 1351,
-                bip66_height: 1251,                    // used only in rpc tests
+                bip34_height: 1,
+                bip65_height: 1,
+                bip66_height: 1,
                 rule_change_activation_threshold: 108, // 75%
                 miner_confirmation_window: 144,
                 pow_limit: Target::MAX_ATTAINABLE_REGTEST,
-                pow_target_spacing: 10 * 60,            // 10 minutes.
-                pow_target_timespan: 14 * 24 * 60 * 60, // 2 weeks.
-                allow_min_difficulty_blocks: true,
-                no_pow_retargeting: true,
+                pow_target_spacing: 12,            // 12 seconds.
+                pow_target_timespan: 2 * 15 * 60,  // 30 min
+                allow_min_difficulty_blocks: true, // POW
+                no_pow_retargeting: false,
             },
         }
     }
